4fe8711bf75e319da4fcb669cf3b6358f0557f5b
Andres <andresduarter13@gmail.com>
2018-11-08 23:22:14 -0400

    Puse bootstrap en gestion de aulas

--- Aulas/GestionDeAulas.php
+++ Aulas/GestionDeAulas.php
@@ -1,7 +1,8 @@
 <html>
 <head>
-  <link href="Style.css" type="text/css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
   <?php
@@ -29,6 +30,11 @@
   <?php
    $dblink->close();
    ?>
+   <!-- jQuery -->
+   <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+   <!-- Bootstrap JS -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 
 </html>
