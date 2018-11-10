bb016fe36334f97beaf73d879950336b014ccda1
andres <andresduarter13@gmail.com>
2018-11-10 17:53:49 -0400

    check boxes en editar aula

--- Aulas/EditarAula.php
+++ Aulas/EditarAula.php
@@ -18,7 +18,7 @@
            . $dblink->connect_error);
    }
 
-   $sql = "select * from aulas;";
+   $sql = "select * from categorias;";
    $result = $dblink->query($sql);
 ?>
 
@@ -32,9 +32,50 @@
   <label for="CantidadDeAlumnos">Cantidad de Alumnos:</label>
   <input type="password" class="form-control" id="pwd">
 </div>
+<div>
+  <table class="table table-striped table-bordered table-responsive-sm m-5">
+    <thead class="thead-dark">
+      <tr>
+        <th style="width: 50%"> Categoria</th>
+        <th style="width: 50%">Hola</th>
+      </tr>
+    </thead>
+    <tbody>
+     <?php   while ($fila = $result->fetch_object()){  ?>
+      <tr>
+         <td><?php echo " $fila->nombre_categoria"; ?></td>
+         <td><?php echo "<input type=\"checkbox\" class=\"form-check-input\" disabled>";?></td>
+      </tr>
 
-  <button><a href="GestionDeAulas.php">Confirmar</a>
+     <?php } ?>
+     <?php
+      $dblink->close();
+      ?>
+   </tbody>
+  </table>
+</div>
+
+  <button><a href="GestionDeAulas.php" id="Confirmar">Confirmar</a>
   </button>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          AIUDA
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
   <?php
    $dblink->close();
    ?>
