f525af7f83c4d831af9f7407a79308e554ab0443
andres <andresduarter13@gmail.com>
2018-11-10 02:01:39 -0400

    Gestion de Aulas, ahora tiene El pop up y no es necesario tener una ventana a parte de elminacion, falta implementar php

--- Aulas/GestionDeAulas.php
+++ Aulas/GestionDeAulas.php
@@ -34,8 +34,27 @@
      <tr>
         <td><?php echo " $fila->nombre"; ?></td>
         <td><?php echo "<a href=\"EditarAula.php\"> Editar";?></td>
-        <td><?php echo "<a href=\"EliminarAula.php\"> Borrar";?></td>
+        <td><?php echo "<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\"> Borrar";?></td>
      </tr>
+     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+       <div class="modal-dialog" role="document">
+         <div class="modal-content">
+           <div class="modal-header">
+             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+             </button>
+           </div>
+           <div class="modal-body">
+             AIUDA
+           </div>
+           <div class="modal-footer">
+             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+             <button type="button" class="btn btn-primary">Save changes</button>
+           </div>
+         </div>
+       </div>
+     </div>
     <?php } ?>
     <?php
      $dblink->close();
