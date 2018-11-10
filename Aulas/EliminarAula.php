43cc35a6a5e3a65462e177f5951eafc3743118a6
andres <andresduarter13@gmail.com>
2018-11-10 01:54:17 -0400

    Lindo pop up en eliminar aulas :D

--- Aulas/EliminarAula.php
+++ Aulas/EliminarAula.php
@@ -1,9 +1,14 @@
 <html>
 <head>
-  <link href="Style.css" type="text/css" rel="stylesheet" />
+  <link rel="stylesheet" href="css/bootstrap.css" >
 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
+  <link href="custom.css" type="text/css" rel="stylesheet" />
+  <button type="button" class="btn btn-danger">Cerrar sesión</button>
+    <a href="GestionDeAulas.php"><img src="Logo_UPB.jpg" class="img-fluid " alt="Responsive image" ></a>
+
   <?php
 
    $db_name = "bd_aulasperronas";
@@ -18,18 +23,39 @@
    $sql = "select * from aulas;";
    $result = $dblink->query($sql);
 ?>
-Aulas:<br>
-<ul>
-  <?php while ($fila = $result->fetch_object()) {
-    echo"<li> $fila->nombre  -  <a href = \"EditarAula.php\">Editar</a> -  <a href = \"BorrarAula.php\">Borrar</a> </li>"
-  ; } ?>
-</ul>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        AIUDA
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   <button><a href="GestionDeAulas.php">Atras</a>
   <button><a href="GestionDeAulas.php">Confirmar</a>
   </button>
   <?php
    $dblink->close();
    ?>
+   <!-- jQuery -->
+   <script src="js/jquery-3.3.1.min.js"></script>
+
+   <!-- Bootstrap JS -->
+   <script src="js/bootstrap.min.js" ></script>
 </body>
 
 </html>
