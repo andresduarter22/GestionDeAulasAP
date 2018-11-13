65167c439cb7c3647db06c2eafed5922781ab181
Luis Fer <pastillalc@gmail.com>
2018-11-12 22:15:49 -0400

    Ordenada Homes

--- Homes/Home.php
+++ Homes/Home.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <link rel="stylesheet" href="css/bootstrap.css" >
+  <link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
   <script src="https://apis.google.com/js/platform.js" async defer></script>
@@ -14,16 +14,16 @@
 
   Bienvenidos al sistema de reservas UPB La Paz
 
-  <img src="Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" >
+  <img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" >
 
 <div class="g-signin2" data-onsuccess="onSignIn">
   <button type="button" class="btn">  Log in GOOGLE </button>
 </div>
    <!-- jQuery -->
-   <script src="js/jquery-3.3.1.min.js"></script>
+   <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
 
    <!-- Bootstrap JS -->
-   <script src="js/bootstrap.min.js" ></script>
+   <script src="../Booststrap/js/bootstrap.min.js" ></script>
 </body>
 
 </html>
