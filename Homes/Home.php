c80de18cdd7518c13a773adfe95d27644d40d574
Luis Fer <pastillalc@gmail.com>
2018-11-15 20:16:47 -0400

    inicio pagina login con jumbotron

--- Homes/Home.php
+++ Homes/Home.php
@@ -3,6 +3,8 @@
   <link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
+
+<!--Google    -->
   <script src="https://apis.google.com/js/platform.js" async defer></script>
 <meta name="google-signin-client_id"
  content="287725469437-1ielsrb28qvp2sv3pa7m1ak8jk9l8816.apps.googleusercontent.com">
@@ -10,15 +12,24 @@
   <title>Home</title>
 </head>
 <body>
-  <button type="button" class="btn btn-danger">Cerrar sesión</button>
+  <div class="jumbotron jumbotron-fluid">
+    <img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" >
+    <div class="container">
+      <h1 class="display-4">Bienvenido al Sistema de Reservas UPB La Paz</h1>
 
-  Bienvenidos al sistema de reservas UPB La Paz
-
-  <img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" >
-
+      <br>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="HomeLogeado.php" role="button">Log in</a>
+      </p>
+    </div>
+  </div>
+<!-- Log In GOOGLE
 <div class="g-signin2" data-onsuccess="onSignIn">
   <button type="button" class="btn">  Log in GOOGLE </button>
 </div>
+-->
+
+
    <!-- jQuery -->
    <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
 
--- Usuarios/CrearUsuario.php
+++ Usuarios/CrearUsuario.php
@@ -56,7 +56,11 @@
     while ($fila = $result->fetch_object()){  ?>
    <tr>
       <td><?php echo " $fila->nombre"; ?></td>
-      <td><?php echo "<input type=\"checkbox\" class=\"form-check-input\" enabled>";?></td>
+      <td><?php  echo "<input type=\"checkbox\" class=\"form-check-input\" enabled>";?>
+    <!--    <form action="#" method="post">
+         <input type="checkbox" name="check_list[]" ><br/>
+       </form> -->
+      </td>
    </tr>
     <?php } ?>
 </table>
@@ -74,7 +78,7 @@
         while ($fila = $result->fetch_object()){  ?>
        <tr>
             <td><?php echo " $fila->nombre_categoria"; ?></td>
-          <td><?php echo "<input type=\"checkbox\" class=\"form-check-input\" enabled>";?></td>
+          <td><?php echo "<input  type=\"checkbox\" class=\"form-check-input\" enabled>";?></td>
        </tr>
         <?php }
         $dblink->close(); ?>
@@ -107,6 +111,9 @@
     $_nombre= $_POST['nombre'];
     $_interno= $_POST['numInt'];
     $_Email= $_POST['correo'];
+   $_aulas= $_POST['check_list'];
+   echo "$_nombre $_interno $_Email $_categoria";
+    echo "$_aulas";
 
     $sql = "insert into Usuarios(id_Usuario,nombre,num_interno,E_Mail,Rol)
                 values(NULL,'$_nombre','$_interno','$_Email','$_categoria')";
