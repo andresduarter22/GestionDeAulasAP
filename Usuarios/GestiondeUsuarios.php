f200477d5dcf0ca27469c40e6b5158bd8d0c668c
Luis Fer <pastillalc@gmail.com>
2018-11-16 16:00:55 -0400

    implementado PDO en Gestion de Usuarios

--- Usuarios/GestiondeUsuarios.php
+++ Usuarios/GestiondeUsuarios.php
@@ -11,19 +11,13 @@
     <button type="button" class="btn btn-danger">Cerrar sesión</button>
     <a href="GestionDeAulas.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right" alt="Responsive image"></a>
       <?php
-
-      session_start();
-       $_SESSION['id'] = null;
-      $db_name = "bd_aulasperronas";
-      $db_user = "root";
-      $db_pass = "";
-      $dblink = new mysqli('localhost', $db_user, $db_pass, $db_name);
-      if ($dblink->connect_error) {
-       die('Error al conectar a la Base de Datos (' .  $dblink->connect_errno . ') '
-             . $dblink->connect_error);
-     }
-      $sql = "select * from usuarios;";
-      $result = $dblink->query($sql);
+        include_once "../Config/Database.php";
+        session_start();
+        $db= new Database();
+        $dblink= $db->getConnection();
+        $sql = 'select * from usuarios;';
+        $result = $dblink->query($sql);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
       ?>
     <div class="container" >
      <table class="table table-striped table-bordered  table-responsive-sm  scrollbar">
@@ -38,11 +32,11 @@
        </tr>
      </thead>
         <tbody>
-         < <?php   while ($fila = $result->fetch_object()){  ?>
+         < <?php   while ($fila = $result->fetch()){  ?>
           <tr>
-             <td><?php echo " $fila->nombre"; ?></td>
-             <td><?php echo "$fila->num_interno";  ?></td>
-             <td><?php echo " $fila->E_Mail"; ?></td>
+             <td><?php echo $fila['nombre']; ?></td>
+             <td><?php echo $fila['num_interno']; ?></td>
+             <td><?php echo $fila['E_Mail']; ?></td>
              <td><?php if ($fila->Rol ==0 ) {
                echo "reservador";
              } elseif ($fila->Rol ==1 ) {
