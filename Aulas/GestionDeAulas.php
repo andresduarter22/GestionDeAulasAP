b244dc7516dc77fa505907fc2845e4ac9b553d97
andres <andresduarter13@gmail.com>
2018-11-17 13:04:29 -0400

    No se que hice haha pero creo que la cgue un poquito LOL

--- Aulas/GestionDeAulas.php
+++ Aulas/GestionDeAulas.php
@@ -36,34 +36,9 @@
     <?php   while ($fila = $result->fetch_object()){  ?>
      <tr>
         <td><?php echo " $fila->nombre"; ?></td>
-<<<<<<< HEAD
-        <td><?php echo "<a href=\"EditarAula.php\" class=\"btn btn-primary\" > Editar";?></td>
-        <td><?php echo "<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\"> Borrar";?></td>
-     </tr>
-     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-       <div class="modal-dialog" role="document">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-             </button>
-           </div>
-           <div class="modal-body">
-             AIUDA
-           </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-             <button type="button" class="btn btn-primary">Save changes</button>
-           </div>
-         </div>
-       </div>
-     </div>
-=======
         <td><?php echo "<a href=\"EditarAula.php\" class=\"btn btn-primary\">Editar";?></td>
         <td><?php echo '<a href= EliminarAula.php ?id_Aulas=$fila["id_Aulas"] class= "btn btn-primary" >Borrar';?></td>
          </tr>
->>>>>>> 83c40007a983d6108000413c871b84e438ba9abf
     <?php } ?>
     <?php
      $dblink->close();
