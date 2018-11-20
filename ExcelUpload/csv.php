378c305435856fada973865e66c8b972a3025d3e
Luis Fer <pastillalc@gmail.com>
2018-11-20 19:46:22 -0400

    excel listo pero falta preguntar

--- ExcelUpload/csv.php
+++ ExcelUpload/csv.php
@@ -1,40 +1,45 @@
 <?php
 
+
 class csv
 {
 
   public function import($file){
     $file = fopen($file, 'r');
-    $cont=1;
+    $_cont=0;
     while ($row = fgetcsv($file)) {
-    
-      if($cont>1){
+      if($_cont>0){
         $numero = count($row);
         for ($c=0; $c < $numero; $c++) {
-        echo $row[$c] . "<br />\n";
-          CrearCaracter($row[$c]);
+          $_cadenaDeDatos=$row[0];
+    //      echo $_cadenaDeDatos   ."</br>";
+    //      $this->CrearCaracter($_cadenaDeDatos);
         }
       }
-      $cont++;
+      $_cont++;
     }
   }
 
   public function CrearCaracter($cadena){
-    $_Materia;
-    $_FechaInicio;
-    $_FechaFinal;
-    $_Horario;
-    $_Aula;
-    $_Docente;
-    $_largo=strlen($cadena);
-    echo "$_largo";
+    //echo $cadena   ."</br>";
+    $myArray = explode(';', $cadena);
 
-  }
+    $_Materia=$myArray[0];
+    $_FechaInicio=$myArray[1];
+    $_FechaFinal=$myArray[2];
+    $_Horario=$myArray[3];
+    $_Aula=$myArray[4];
+    $_Docente=$myArray[7];
+    /*print_r($myArray);
+    echo "</br>";*/
 
+
+    include "../Config/Database.php";
+    $db= new Database();
+    $dblink= $db->getConnection();
+    $sql = 'select * from usuarios;';
+    $result = $dblink->query($sql);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+ }
 }
-
-
-
-
-
   ?>
