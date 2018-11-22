2c944e9ae555ee9cbc9db0be5d5bef6b709d296b
Luis Fer <pastillalc@gmail.com>
2018-11-21 22:47:06 -0400

    clase Excel up Sube excel a la base con excel.csv

--- ExcelUpload/csv.php
+++ ExcelUpload/csv.php
@@ -12,14 +12,72 @@ class csv
         for ($c=0; $c < $numero; $c++) {
           $_cadenaDeDatos=$row[0];
     //      echo $_cadenaDeDatos   ."</br>";
-         $this->AgregarMaterias($_cadenaDeDatos);
+         $this->AgregarReservasAutomaticas($_cadenaDeDatos);
         }
       }
       $_cont++;
     }
   }
 
-  //funcion que agrega solo aulas
+  public function AgregarReservasAutomaticas($cadena){
+    $myArray = explode(';', $cadena);
+
+    $_Materia=$myArray[0];
+    $_FechaInicio=$myArray[1];
+    $_FechaFinal=$myArray[2];
+    $_Horario=$myArray[3];
+    $_Aula=$myArray[4];
+    $_Docente=$myArray[7];
+
+    echo "$_Materia $_FechaInicio $_FechaFinal $_Horario $_Aula $_Docente </br>";
+    include_once "../Config/Database.php";
+    $db= new Database();
+    $dblink= $db->getConnection();
+
+    // Verificar si exite aula
+    $sql = "SELECT * FROM aulas WHERE nombre= '$_Aula';";
+    $result = $dblink->query($sql);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+    if($result->rowCount()){
+      echo "existe Aula</br>";
+      echo "insertando... </br>";
+      //obteniendo id de Materia
+      $sql = "SELECT id_Materias from materias WHERE nombre_materia='$_Materia';";
+      $result = $dblink->query($sql);
+      $result->setFetchMode(PDO::FETCH_ASSOC);
+      $_IdMateria = $result->fetchColumn();
+      //echo "$_IdMateria";
+      //echo "  ";
+      //obteniendo id de Aula
+      $sql = "SELECT id_Aulas from aulas WHERE nombre='$_Aula';";
+      $result = $dblink->query($sql);
+      $result->setFetchMode(PDO::FETCH_ASSOC);
+      $_IdAula = $result->fetchColumn();
+      //echo "$_IdAula";
+
+      //ordenando fechas
+      $ArregloFechaIni = explode('/', $_FechaInicio);
+      $ArregloFechaFin = explode('/', $_FechaFinal);
+
+      $_FInicial= $ArregloFechaIni[2].'-'.$ArregloFechaIni[1].'-'.$ArregloFechaIni[0];
+      $_FFinal=$ArregloFechaFin[2].'-'.$ArregloFechaFin[1].'-'.$ArregloFechaFin[0];
+      //echo "$_FInicial";
+      //echo "$_FFinal";
+
+      //insertando en la base
+      $sql = "INSERT INTO reservas values(NULL,$_IdAula,2,$_IdMateria,'$_FInicial','$_FFinal',0,'$_Horario');";
+      if ($dblink->query($sql) === FALSE) {
+        echo "Error: " . $sql . "<br>" . $dblink->error;
+      }
+
+    }else {
+      echo "no existe Aula";
+    }
+    echo "</br>";
+    $dblink=null;
+
+}
+
   /**
     FALTA EVITAR QUE SE LEEA LA ULTIMA LINEA
   */
