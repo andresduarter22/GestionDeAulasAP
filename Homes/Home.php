472f94d60ab2274c91562245d69a1ca792865bfb
Luis Fer <pastillalc@gmail.com>
2019-01-02 23:51:01 -0400

    arreglo de inputs usuarios

--- Homes/Home.php
+++ Homes/Home.php
@@ -1,78 +1,78 @@
 <?php
-  $_IDUsuarioTemporal=3;
- ?>
+$_IDUsuarioTemporal = 3;
+?>
 
 <html>
 <head>
-  <!-- jQuery -->
-  <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
+    <!-- jQuery -->
+    <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
 
-  <!-- Bootstrap JS -->
-  <script src="../Booststrap/js/bootstrap.min.js" ></script>
-  <link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!-- Bootstrap JS -->
+    <script src="../Booststrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../Booststrap/css/bootstrap.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 
-<!--Google    -->
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
-<meta name="google-signin-client_id"
- content="287725469437-1ielsrb28qvp2sv3pa7m1ak8jk9l8816.apps.googleusercontent.com">
-  <title>Home</title>
+    <!--Google    -->
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id"
+          content="287725469437-1ielsrb28qvp2sv3pa7m1ak8jk9l8816.apps.googleusercontent.com">
+    <title>Home</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="HomeLogeado.php?id=<?php echo $_IDUsuarioTemporal; ?> ">Log In</a>
 
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="../Busqueda/MotorDeBusqueda.php"> Consultar Reserva
-          <span class="sr-only">(current)</span></a>
-        </li>
-      </ul>
-    </div>
-  </nav>
 
-  <div class="jumbotron jumbotron-fluid">
-    <img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" >
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+    <img src="../Images/Logo_UPB.jpg" class="img-fluid float-right" alt="Responsive image">
     <div class="container">
-      <h1 class="display-4">Bienvenido al Sistema de Reservas UPB La Paz</h1>
+        <h1 class="display-4">Bienvenido al Sistema de Reservas UPB La Paz</h1>
+        <div class="container">
+            <br>
+            <a class="btn btn-success" href="../Busqueda/MotorDeBusqueda.php" role="button">Realizar Consulta</a>
 
-  <!--      <a class="btn btn-primary btn-lg" href="HomeLogeado.php" role="button">Log </a>
-        <a class="btn btn-success" href="HomeLogeado.php" role="button">Consultar Reserva</a> -->
+            <!--      <a class="btn btn-primary btn-lg" href="HomeLogeado.php" role="button">Log </a>
+                  <a class="btn btn-success" href="HomeLogeado.php" role="button">Consultar Reserva</a> -->
+        </div>
     </div>
-  </div>
-  <!-- Inicio boton de informacion -->
-  <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img  src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image" height="42" width="42"  data-target="info"/></button>
-  <div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Informacion</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <!-- Inicio boton de informacion -->
+    <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img
+                src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image"
+                height="42" width="42" data-target="info"/></button>
+    <div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Informacion</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Bienvenido al sistema de reserva de aulas UPB La Paz, en esta pagina puede logearse como usuario o
+                    realizar una consulta
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-          Esta es la pantalla donde se puede consultar toda la lista de Aulas dentro de la base de Datos
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-        </div>
-      </div>
     </div>
-  </div>
-  <!-- Final boton de Informacion -->
+    <!-- Final boton de Informacion -->
 
-<!-- Log In GOOGLE-->
-<div class="g-signin2" data-onsuccess="onSignIn">
-  <button type="button" class="btn">  Log in GOOGLE </button>
-</div>
+    <!-- Log In GOOGLE-->
+    <div class="g-signin2" data-onsuccess="onSignIn">
+        <button type="button" class="btn"> Log in GOOGLE</button>
+    </div>
 
 </body>
 
--- Usuarios/CrearUsuario.php
+++ Usuarios/CrearUsuario.php
@@ -1,216 +1,215 @@
 <?php
 include "../Config/Database.php";
-$corrErr = $NumintErr = $NombreErr =$CatErr ="";
-if(isset($_POST['creaUsuario'])){
-  if (empty($_POST["Categoria"])) {
-    $CatErr = "Categoria es requerida";
-  }
+$corrErr = $NumintErr = $NombreErr = $CatErr = "";
+if (isset($_POST['creaUsuario'])) {
 
-  if (empty($_POST["nombre"])) {
-    $NombreErr = "Nombre es requerido";
-  }else {
     $name = $_POST["nombre"];
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $NombreErr = "Solo letras y espacios requeridos";
+    if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+        $NombreErr = "Solo letras y espacios requeridos";
     }
-  }
-  if (empty($_POST["numInt"])) {
-    $NumintErr = "Numero de Interno es Requerido";
-  }else {
-    $name = $_POST["numInt"];
-    if (!is_numeric($name)) {
-      $NumintErr = "Solo numeros en como numero de interno ";
-    }
-  }
-  if (empty($_POST["correo"])) {
-    $corrErr = "Correo es requerido";
-  }else {
+
     $email = $_POST["correo"];
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $corrErr = "Formato de Correo invalido ";
+        $corrErr = "Formato de Correo invalido ";
     }
-  }
 
-  if (empty($corrErr) && empty($CatErr) && empty($NumintErr) && empty($NombreErr)){
-    create();
-  }
+    if (empty($corrErr) && empty($NombreErr)) {
+        create();
+        echo '<script language="javascript">';
+        echo 'alert("Usuario Correctamente agregado")';
+        echo '</script>';
+    }
 }
- ?>
+?>
 
-<html>
+<html xmlns:overflow="http://www.w3.org/1999/xhtml">
 <head>
-  <!-- jQuery -->
-  <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
+    <!-- jQuery -->
+    <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
 
-  <!-- Bootstrap JS -->
-  <script src="../Booststrap/js/bootstrap.min.js" ></script>
-  <link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Crear Usuarios</title>
+    <!-- Bootstrap JS -->
+    <script src="../Booststrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../Booststrap/css/bootstrap.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Crear Usuarios</title>
 </head>
 <body>
-  <button type="button" class="btn btn-danger">Cerrar sesión</button>
-    <a href="GestionDeUsuarios.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"  alt="Responsive image" ></a>
-    <?php
-      //Conexion con base
-      // se crea una nueva instancia de la clase
-      $db= new Database();
-      // se llama a la conexion, caulquier cosa que se quiera hacer con la base se llama a esa variable
-      $dblink= $db->getConnection();
-    ?>
+<button type="button" class="btn btn-danger">Cerrar sesión</button>
+<a href="GestionDeUsuarios.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right" alt="Responsive image"></a>
+<?php
+//Conexion con base
+// se crea una nueva instancia de la clase
+$db = new Database();
+// se llama a la conexion, caulquier cosa que se quiera hacer con la base se llama a esa variable
+$dblink = $db->getConnection();
+?>
 <form action="CrearUsuario.php" method="post">
-  <div class="container" >
-    <select class="custom-select" name="Categoria">
-      <option selected>Categoria de Usuario</option>
-      <option value="1">Administrador</option>
-      <option value="2">Actualizador</option>
-      <option value="3">Reservador</option>
-    </select>
-      <span class="error"> <?php echo $CatErr;?></span>
-    <div class="form-group">
-      <label for="NombreUsuario">Nombre:</label>
-      <input type="text" class="form-control" id="usr" name="nombre">
-      <span class="error"> <?php echo $NombreErr;?></span>
-    </div>
-    <div class="form-group">
-      <label for="num_interno">Numero de Interno:</label>
-      <input type="text" class="form-control" id="interno" name="numInt">
-      <span class="error"> <?php echo $NumintErr;?></span>
-    </div>
-    <div class="form-group">
-      <label for"E_Mail">E-Mail:</label>
-      <input type="text" class="form-control" id="e_mail" name="correo">
-      <span class="error"> <?php echo $corrErr;?></span>
-    </div>
-    <table class="table table-striped table-bordered  table-responsive-sm m-5s">
-      <thead  class="thead-dark">
-        <tr>
-          <th style="width: 15%">Nombre de aula </th>
-          <th style="width: 15%">Check </th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          // se crea el query
-          $sql = 'select * from aulas order by nombre;';
-          //  $result tiene el resultado de la busqueda
-          $result = $dblink->query($sql);
-          while ($fila = $result->fetch()){
-        ?>
-        <tr>
-          <td><?php echo $fila['nombre'] ?></td>
-          <td><?php  echo "<input type=\"checkbox\" name=\"aula[]\" id=\"aula\" value=\" ".$fila['id_Aulas']." \" enabled>";?> </td>
-        </tr>
-        <?php } ?>
-    </table>
-  <table class="table table-striped table-bordered  table-responsive-sm m-5s">
-      <thead  class="thead-dark">
-        <tr>
-          <th style="width: 15%">Nombre de categoria </th>
-          <th style="width: 15%">Check </th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $sql = "select * from categorias;";
-          $result = $dblink->query($sql);
-          $result->setFetchMode(PDO::FETCH_ASSOC);
-          while ($fila = $result->fetch()){
-        ?>
-      <tr>
-        <td><?php echo $fila['nombre_categoria']  ?></td>
-        <td><?php echo "<input  type=\"checkbox\" name=\"categoria[]\" id=\"categoria\" value=\" ".$fila['id_Categorias']." \" enabled>";?></td>
-      </tr>
-        <?php }
-        ?>
-    </table>
-  </div>
+    <div class="container">
+        <select class="custom-select" name="Categoria" style="width: 300px" required>
+            <option disabled selected value> -- Seleccione una Categoria  -- </option>
+            <option value="1">Administrador</option>
+            <option value="2">Actualizador</option>
+            <option value="3">Reservador</option>
+        </select>
+        <span class="error"> <?php echo $CatErr; ?></span>
+        <div class="form-group" style="width: 300px">
+            <label for="NombreUsuario">Nombre:</label>
+            <input type="text" class="form-control" id="usr" name="nombre" required>
+            <span class="error alert-danger" > <?php echo $NombreErr; ?></span>
+        </div>
+        <div class="form-group" style="width: 300px">
+            <label for="num_interno">Numero de Interno:</label>
+            <input type="number" class="form-control" id="interno" name="numInt" required>
+            <span class="error alert-danger"> <?php echo $NumintErr; ?></span>
+        </div>
 
-  <form action="CrearUsuario.php" method="post">
-    <input type="submit" name="creaUsuario" value="submit">
-  </form>
+        <div class="form-group" style="width: 300px">
+            <label for"E_Mail">E-Mail:</label>
+            <input type="text" class="form-control" id="e_mail" name="correo" required>
+            <span class="error alert-danger"> <?php echo $corrErr; ?></span>
+        </div>
 
-  <a class="btn btn-primary" href="GestionDeUsuarios.php">Atras</a>
-  </form>
-  <!-- Inicio boton de informacion -->
-  <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img  src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image" height="42" width="42"  data-target="info"/></button>
-  <div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="row">
+            <div class="scro">
+                <table class="table table-fixed table-striped table-bordered  table-responsive-sm m-5s ">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th style="width: 15%">Nombre de aula</th>
+                        <th style="width: 15%">Check</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    // se crea el query
+                    $sql = 'select * from aulas order by nombre;';
+                    //  $result tiene el resultado de la busqueda
+                    $result = $dblink->query($sql);
+                    while ($fila = $result->fetch()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $fila['nombre'] ?></td>
+                            <td><?php echo "<input type=\"checkbox\" name=\"aula[]\" id=\"aula\" value=\" " . $fila['id_Aulas'] . " \" enabled>"; ?> </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <hr>
+            <div class="scro">
+                <table class="table table-striped table-bordered  table-responsive-sm m-5s">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th style="width: 15%">Nombre de categoria</th>
+                        <th style="width: 15%">Check</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $sql = "select * from categorias;";
+                    $result = $dblink->query($sql);
+                    $result->setFetchMode(PDO::FETCH_ASSOC);
+                    while ($fila = $result->fetch()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $fila['nombre_categoria'] ?></td>
+                            <td><?php echo "<input  type=\"checkbox\" name=\"categoria[]\" id=\"categoria\" value=\" " . $fila['id_Categorias'] . " \" enabled>"; ?></td>
+                        </tr>
+                    <?php }
+                    ?>
+                </table>
+            </div>
+        </div>
+        <br>
+        <form action="CrearUsuario.php" method="post">
+            <input type="submit" name="creaUsuario" value="Crear Usuario">
+        </form>
+    </div>
+
+
+    <a class="btn btn-primary" href="GestionDeUsuarios.php">Atras</a>
+</form>
+<!-- Inicio boton de informacion -->
+<button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img
+            src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image"
+            height="42" width="42" data-target="info"/></button>
+<div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          Esta es la pantalla donde se puede consultar toda la lista de Aulas dentro de la base de Datos
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Esta es la pantalla donde se puede consultar toda la lista de Aulas dentro de la base de Datos
 
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 <?php
 
-  function create(){
-    $db= new Database();
-    $dblink= $db->getConnection();
-    $_categoriaUsuario= $_POST['Categoria'];
-    $_nombre= $_POST['nombre'];
-    $_interno= $_POST['numInt'];
-    $_Email= $_POST['correo'];
-    $_aulas= $_POST['aula'];
-    $_categorias= $_POST['categoria'];
+function create()
+{
+    $db = new Database();
+    $dblink = $db->getConnection();
+    $_categoriaUsuario = $_POST['Categoria'];
+    $_nombre = $_POST['nombre'];
+    $_interno = $_POST['numInt'];
+    $_Email = $_POST['correo'];
+    $_aulas = $_POST['aula'];
+    $_categorias = $_POST['categoria'];
 
-  $sql = "insert into Usuarios(id_Usuario,nombre,num_interno,E_Mail,Rol) values(NULL,'$_nombre','$_interno','$_Email','$_categoriaUsuario')";
-  if ($dblink->query($sql) === FALSE) {
-    echo "Error: " . $sql . "<br>" . $dblink->error;
-  }
-  $_idUsuarioCreado=$dblink->lastInsertId();
-
-  //revisa todas las categorias y saca la lista de los id de aulas
-  foreach ($_categorias as  $value) {
-    $sql2 = "insert into usuarios_categorias values(NULL,'$_idUsuarioCreado','$value')";
-    if ($dblink->query($sql2) === FALSE) {
-      echo "Error: " . $sql2 . "<br>" . $dblink->error;
-    }
-
-    /*
-     * Esta selecciona el id de Aula para cada categoria y la vuelve a ingresar a la tabla usuarios_aulas cada uno de
-     * los ids de aula que encuentra
-     *
-     *
-    $sql = "select id_Aula from aulas_categoria where id_Categoria = '$value' " ;
+    $sql = "insert into Usuarios(id_Usuario,nombre,num_interno,E_Mail,Rol) values(NULL,'$_nombre','$_interno','$_Email','$_categoriaUsuario')";
     if ($dblink->query($sql) === FALSE) {
-      echo "Error: " . $sql . "<br>" . $dblink->error;
+        echo "Error: " . $sql . "<br>" . $dblink->error;
     }
-    $result = $dblink->query($sql);
-    $result->setFetchMode(PDO::FETCH_ASSOC);
-    while ($fila = $result->fetch()){
-    //  echo $fila['id_Aula'];
-      $valorIDDeAula= $fila['id_Aula'];
-      $sql2 = "insert into usuarios_aulas(idUsuarios_Aulas,id_DeAula,id_DeUsuario) values(NULL,'$valorIDDeAula','$_idUsuarioCreado')";
-      if ($dblink->query($sql2) === FALSE) {
-        echo "Error: " . $sql2 . "<br>" . $dblink->error;
-      }
-    }*/
-  }
+    $_idUsuarioCreado = $dblink->lastInsertId();
 
+    //revisa todas las categorias y saca la lista de los id de aulas
+    foreach ($_categorias as $value) {
+        $sql2 = "insert into usuarios_categorias values(NULL,'$_idUsuarioCreado','$value')";
+        if ($dblink->query($sql2) === FALSE) {
+            echo "Error: " . $sql2 . "<br>" . $dblink->error;
+        }
 
-  foreach ($_aulas as  $value) {
-    $sql = "insert into usuarios_aulas(idUsuarios_Aulas,id_DeAula,id_DeUsuario) values(NULL,'$value','$_idUsuarioCreado')";
-    if ($dblink->query($sql) === FALSE) {
-      echo "Error: " . $sql . "<br>" . $dblink->error;
+        /**
+         * Esta selecciona el id de Aula para cada categoria y la vuelve a ingresar a la tabla usuarios_aulas cada uno de
+         * los ids de aula que encuentra
+         * */
+        /*
+        $sql = "select id_Aula from aulas_categoria where id_Categoria = '$value' " ;
+        if ($dblink->query($sql) === FALSE) {
+          echo "Error: " . $sql . "<br>" . $dblink->error;
+        }
+        $result = $dblink->query($sql);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        while ($fila = $result->fetch()){
+        //  echo $fila['id_Aula'];
+          $valorIDDeAula= $fila['id_Aula'];
+          $sql2 = "insert into usuarios_aulas(idUsuarios_Aulas,id_DeAula,id_DeUsuario) values(NULL,'$valorIDDeAula','$_idUsuarioCreado')";
+          if ($dblink->query($sql2) === FALSE) {
+            echo "Error: " . $sql2 . "<br>" . $dblink->error;
+          }
+        }*/
     }
-  }
-  $sql_log_cu = "INSERT INTO Logs (id_Log,nombre_usuario,num_interno_usuario,correo_usuario,tipo_usuario,Accion,Fecha_Accion) VALUES (NULL,'Andres','666','ad@gmail.com','m','Se creo un usuario llamado $_nombre',now())";
-  $dblink->query($sql_log_cu);
-  }
 
+
+    foreach ($_aulas as $value) {
+        $sql = "insert into usuarios_aulas(idUsuarios_Aulas,id_DeAula,id_DeUsuario) values(NULL,'$value','$_idUsuarioCreado')";
+        if ($dblink->query($sql) === FALSE) {
+            echo "Error: " . $sql . "<br>" . $dblink->error;
+        }
+    }
+    $sql_log_cu = "INSERT INTO Logs (id_Log,nombre_usuario,num_interno_usuario,correo_usuario,tipo_usuario,Accion,Fecha_Accion) VALUES (NULL,'Andres','666','ad@gmail.com','m','Se creo un usuario llamado $_nombre',now())";
+    $dblink->query($sql_log_cu);
+}
 
 
 ?>
