b725645007521e44c3b3658d77a7de9c02313eb5
Luis Fer <pastillalc@gmail.com>
2019-01-04 14:26:17 -0400

    calendario con multiples clicks :D

--- Busqueda/MotorDeBusqueda.php
+++ Busqueda/MotorDeBusqueda.php
@@ -1,140 +1,137 @@
 <?php
-  include "../Config/Database.php";
+include "../Config/Database.php";
 
 ?>
 <html>
 <head>
-  <!-- jQuery -->
-  <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
+    <!-- jQuery -->
+    <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
+    <script src="jquery-ui-1.12.1/jquery-ui.min.js"></script>
+    <script src="jquery-ui.multidatespicker.js"></script>
 
-  <!-- Bootstrap JS -->
-  <script src="../Booststrap/js/bootstrap.min.js" ></script>
-<link href="Style.css" type="text/css" rel="stylesheet" />
-<link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!-- Bootstrap JS -->
+    <script src="../Booststrap/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="jquery-ui.multidatespicker.css">
+    <link rel="stylesheet" href="jquery-ui-1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" href="../Booststrap/css/bootstrap.css">
+
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
-
-  <div>
+<div>
     <button type="button" class="btn btn-danger">Cerrar sesión</button>
-      <a href="../Homes/HomeLogeado.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right" alt="Responsive image" ></a>
-  </div>
-  <?php
-    //Conexion con base
-    session_start();
-    $db= new Database();
-    $dblink= $db->getConnection();
-    $sql = 'select * from Aulas;';
-    $result = $dblink->query($sql);
-    $result->setFetchMode(PDO::FETCH_ASSOC);
-  ?>
-  <div class="container">
-    <input id="date" type="date"  min="today" onclick="fecha()">
-
-    <p id="demo"></p>
-  <script>
-    function fecha() {
-      <?php $fecha_prueba =  'document.getElementById("date").value' ; ?>
-      <?php echo $fecha_prueba; ?>
-      document.getElementById("demo").innerHTML = x;
-    }
-  </script>
-  </div>
-  <form action="Resultados.php" method="post">
+    <a href="../Homes/HomeLogeado.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"
+                                            alt="Responsive image"></a>
+</div>
+<?php
+//Conexion con base
+session_start();
+$db = new Database();
+$dblink = $db->getConnection();
+$sql = 'select * from Aulas order by nombre;';
+$result = $dblink->query($sql);
+$result->setFetchMode(PDO::FETCH_ASSOC);
+?>
+<form action="Resultados.php" method="post">
     <div class="container" style="width:900px;">
-      <div class="container row">
-        <div class="row">
-          <div class="col-sm-6">
-            <input type="radio" enabled>Dias Especificos</input>
-            <input type="radio" enabled>Dias Seguidos</input>
-          </div>
-            <select class="form-control col-xs-3 ">
-              <?php   while ($fila = $result->fetch()){  ?>
-                <option value="volvo"><?php echo $fila['nombre']; ?></option>
-              <?php } ?>
+        <div class="col-sm-6">
+            <label for="radio-1">Dias Especificos</label>
+            <input value="1" type="radio" name="radio-1" id="radio-1">
+            <label for="radio-2">Dias Seguidos</label>
+            <input value="2" type="radio" name="radio-1" id="radio-2">
+        </div>
+        <input id="date" name="fechas" placeholder="Ingrese fechas">
+        <!-- Actually works! implement something with jquery UI -->
+        <script>
+            $('#date').multiDatesPicker();
+        </script>
+
+        <select class="form-control col-xs-3" name="idDeAula" style="width: 400px">
+            <option disabled selected value> -- Seleccione una aula especifica --</option>
+            <?php while ($fila = $result->fetch()) { ?>
+                <option value=<?php echo $fila['id_Aulas']; ?>><?php echo $fila['nombre']; ?></option>
+            <?php } ?>
+        </select>
+
+        <div class="input-group-prepend">
+            <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input" name="AulaEspecifica">
+            </div>
+            <h6>Aula especifica</h6>
+        </div>
+
+        <input type="text" name="cantalumnos" class="form-control" aria-label="Text input with checkbox" multiple
+               placeholder="Cantidad de Alumnos" style="width: 400px">
+        Horario
+        <div clkass="row-fluid">
+            <select class="selectpicker" id="horario" name="horario" data-live-search="true">
+                <option disabled selected value> -- </option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+                <option value="Z">Z</option>
+                <option value="Z1">Z1</option>
             </select>
-          </div>
         </div>
-<br />
-      <?php
-      /*include 'FuncionCalendario.php';
-
-      $calendar = new Calendar();
-
-      echo $calendar->show();*/
-      ?>
-          <div class="input-group-prepend">
-          <!--<div class="input-group-text">
-            <input type="checkbox" aria-label="Checkbox for following text input">
-          </div>-->
-        </div>
-        <input type="text" name="cantalumnos" class="form-control" aria-label="Text input with checkbox" placeholder="Cantidad de Alumnos">
-        Horarios
-        <div class="row-fluid">
-          <select class="selectpicker"  id="horario" name="horario" data-live-search="true">
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
-            <option value="Z">Z</option>
-            <option value="Z1">Z1</option>
-          </select>
-        </div>
-
-        <table class=" table table-striped table-bordered  table-responsive-sm m-5 scrollbar " >
-          <thead  class="thead-dark">
+        <table class=" table table-striped table-bordered  table-responsive-sm m-5 scrollbar ">
+            <thead class="thead-dark">
             <tr>
-              <th style="width: 30%"> Nombre de la Categoria</th>
-              <th style="width: 10%"> Check </th>
+                <th style="width: 30%"> Nombre de la Categoria</th>
+                <th style="width: 10%"> Check</th>
             </tr>
-          </thead>
-          <tbody>
+            </thead>
+            <tbody>
             <?php
             $sql = 'select * from Categorias;';
             $result = $dblink->query($sql);
-            while ($fila = $result->fetch()){  ?>
-              <tr>
-                <td><?php echo $fila['nombre_categoria']; ?></td>
-                <td><?php echo "<input  type=\"checkbox\" name=\"categoria[]\" id=\"categoria\" value=\" ".$fila['id_Categorias']." \" enabled>";?></td>
-              </tr>
+            while ($fila = $result->fetch()) { ?>
+                <tr>
+                    <td><?php echo $fila['nombre_categoria']; ?></td>
+                    <td><?php echo "<input  type=\"checkbox\" name=\"categoria[]\" id=\"categoria\" value=\" " . $fila['id_Categorias'] . " \" enabled>"; ?></td>
+                </tr>
             <?php } ?>
-          </tbody>
+            </tbody>
         </table>
 
-  <!-- boton para ir atras-->
+        <!-- boton para ir atras-->
 
-      <form action="MotorDeBusqueda.php" method="post">
         <input type="submit" class="btn btn-primary" name="startSearch" value="Buscar">
-      </form>
+
     </div>
-    </form>
-    <a href="../Homes/HomeLogeado.php" class="btn btn-primary">Atras</a>
+</form>
+<a href="../Homes/HomeLogeado.php" class="btn btn-primary">Atras</a>
 
 <!--boton de informacion-->
-    <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img  src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image" height="42" width="42"  data-target="info"/></button>
-    <div class="modal fade" id="info" name="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+<button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img
+            src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image"
+            height="42" width="42" data-target="info"/></button>
+<div class="modal fade" id="info" name="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            AIUDA XD
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                AIUDA XD
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
-      </div>
     </div>
-    <?php
-     $dblink->close();
-     ?>
+</div>
+<?php
+$dblink->close();
+?>
 </body>
 </html>
--- Busqueda/Resultados.php
+++ Busqueda/Resultados.php
@@ -1,195 +1,209 @@
 <?php
 
-    include "../Config/Database.php";
-    include "search.php";
-    session_start();
-    $db= new Database();
-    $dblink= $db->getConnection();
-    $idDeUsuarioReservador=6;
+include "../Config/Database.php";
+include "search.php";
+session_start();
+$db = new Database();
+$dblink = $db->getConnection();
+$idDeUsuarioReservador = 6;
 
-    $se=new search();
-    $arregDisp=0;
-    if(isset($_POST['startSearch'])){
-      $arregDisp=$se->busca();
-    }
+$se = new search();
+$arregDisp = 0;
+if (isset($_POST['startSearch'])) {
+    echo "envio";
+    echo $_POST['fechas'];
+    echo $_POST['radio-1'];
+    echo $_POST['idDeAula'];
+    echo $_POST['AulaEspecifica'];
+    echo $_POST['horario'];
+    //$arregDisp=$se->busca();
+}
 
-    //echo implode(",",$data);
-    //echo $data;
+//echo implode(",",$data);
+//echo $data;
 
-    //$_resultadosDisp = $_GET['disp'];
-    //$_resultadosNoDisp = $_GET['nodisp'];
+//$_resultadosDisp = $_GET['disp'];
+//$_resultadosNoDisp = $_GET['nodisp'];
 
 
-    $_resultadosDisp = $arregDisp[0];
-    $_resultadosNoDisp = $arregDisp[1];
-  //  echo $_resultadosDisp;
+$_resultadosDisp = $arregDisp[0];
+$_resultadosNoDisp = $arregDisp[1];
+//  echo $_resultadosDisp;
 
- ?>
+?>
 
 <html>
-  <head>
+<head>
     <!-- jQuery -->
     <script src="../Booststrap/js/jquery-3.3.1.min.js"></script>
 
     <!-- Bootstrap JS -->
-    <script src="../Booststrap/js/bootstrap.min.js" ></script>
-    <link rel="stylesheet" href="../Booststrap/css/bootstrap.css" >
+    <script src="../Booststrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../Booststrap/css/bootstrap.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Resultados de Busqueda</title>
-  </head>
-  <body>
-    <div>
-        <a href="../Homes/HomeLogeado.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right" alt="Responsive image" ></a>
+</head>
+<body>
+<div>
+    <a href="../Homes/HomeLogeado.php"><img src="../Images/Logo_UPB.jpg" class="img-fluid float-right"
+                                            alt="Responsive image"></a>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="Home.php">Log Out</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
     </div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="Home.php">Log Out</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="../Usuarios/GestiondeUsuarios.php">Usuarios</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-      </div>
-      <a class="navbar-brand" href="../Usuarios/GestiondeUsuarios.php">Usuarios</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    </button>
+    <a class="navbar-brand" href="../Categorias/GestionDeCategorias.php">Categorias</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="../Categorias/GestionDeCategorias.php">Categorias</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="../Aulas/GestionDeAulas.php">Aulas</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-    </nav>
-    <?php // echo implode(",",$_resultadosNoDisp[1]); ?>
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <p class="display-5">Resultados De Busqueda  </p>
-          <div class="container" >
+    </button>
+    <a class="navbar-brand" href="../Aulas/GestionDeAulas.php">Aulas</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
+<?php // echo implode(",",$_resultadosNoDisp[1]); ?>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <p class="display-5">Resultados De Busqueda </p>
+        <div class="container">
             Aulas Diponibles
             <table class="table table-striped table-bordered  table-responsive-sm m-5s">
-              <thead  class="thead-dark">
+                <thead class="thead-dark">
                 <tr>
-                  <th style="width: 40%">Nombre de aula </th>
-                  <th style="width: 30%">Reservar </th>
+                    <th style="width: 40%">Nombre de aula</th>
+                    <th style="width: 30%">Reservar</th>
                 </tr>
-              </thead>
-              <tbody>
+                </thead>
+                <tbody>
                 <?php
-                for ($i=0; $i <count($_resultadosDisp) ; $i++) {
-                  //  echo "$valor";
-                  $sql = "SELECT * FROM aulas WHERE id_Aulas= $_resultadosDisp[$i] ";
-                  $result = $dblink->query($sql);
-                  $infoAulas=$result->fetch();
+                for ($i = 0; $i < count($_resultadosDisp); $i++) {
+                    //  echo "$valor";
+                    $sql = "SELECT * FROM aulas WHERE id_Aulas= $_resultadosDisp[$i] ";
+                    $result = $dblink->query($sql);
+                    $infoAulas = $result->fetch();
 
-                  $sql = "SELECT * FROM usuarios_aulas WHERE id_DeAula= $_resultadosDisp[$i] AND id_DeUsuario = $idDeUsuarioReservador ;";
-                  $infoUsCatego = $dblink->query($sql);
-                //  echo "$sql";
+                    $sql = "SELECT * FROM usuarios_aulas WHERE id_DeAula= $_resultadosDisp[$i] AND id_DeUsuario = $idDeUsuarioReservador ;";
+                    $infoUsCatego = $dblink->query($sql);
+                    //  echo "$sql";
                     echo "<tr>";
-                         //<input type=\"hidden\" name=\"id_AulasParaReservar\" value= " . $infoAulas[0] . " >
-                      if($infoUsCatego->rowCount()){
-                          $_SESSION["id_AulasParaReservar"]=$infoAulas[0];
-                          $_SESSION["id_UsuarioQueReserva"]=$idDeUsuarioReservador;
-                          $_SESSION["fechas"]=$se->_fechasArray;
-                          $_SESSION["tipoDeReserva"]=$se->_tipoDeReserva ;
-                          $_SESSION["horario"]=$se->_horario;
+                    //<input type=\"hidden\" name=\"id_AulasParaReservar\" value= " . $infoAulas[0] . " >
+                    if ($infoUsCatego->rowCount()) {
+                        $_SESSION["id_AulasParaReservar"] = $infoAulas[0];
+                        $_SESSION["id_UsuarioQueReserva"] = $idDeUsuarioReservador;
+                        $_SESSION["fechas"] = $se->_fechasArray;
+                        $_SESSION["tipoDeReserva"] = $se->_tipoDeReserva;
+                        $_SESSION["horario"] = $se->_horario;
 
-                          echo "<td class=\"table-success\">" . $infoAulas[1] . "</td> ";
-                          echo "<td class=\"table-success\">
+                        echo "<td class=\"table-success\">" . $infoAulas[1] . "</td> ";
+                        echo "<td class=\"table-success\">
                           <form method=\"POST\" action= \"ConfrimReserva.php\">
                                 
                           
                             <input type=\"submit\" value=\"Realizar Reserva\" />
                           </form>
                          </td>";
-                      }else {
+                    } else {
                         echo "<td class=\"table-danger\">" . $infoAulas[1] . "</td> ";
                         echo "<td class=\"table-danger\">
                               <form action= \"AutoComplete.php\">
                                 <input type=\"submit\" value=\"RTest\" />
                                 </form>
                          </td>";
-                      }
+                    }
                     echo "</tr>";
                 }
                 ?>
-              </table>
-          </div>
-          <div class="container" >
+            </table>
+        </div>
+        <div class="container">
             Aulas Reservadas
             <table class="table table-striped table-bordered  table-responsive-sm m-5s">
-              <thead  class="thead-dark">
+                <thead class="thead-dark">
                 <tr>
-                  <th style="width: 40%">Nombre de aula </th>
-                  <th style="width: 30%">Informacion</th>
+                    <th style="width: 40%">Nombre de aula</th>
+                    <th style="width: 30%">Informacion</th>
                 </tr>
-              </thead>
-              <tbody>
-                  <?php
-                    for ($i=0; $i <count($_resultadosNoDisp) ; $i++) {
-                      echo "<tr>";
+                </thead>
+                <tbody>
+                <?php
+                for ($i = 0; $i < count($_resultadosNoDisp); $i++) {
+                    echo "<tr>";
 
-                      for ($j=0; $j<  count($_resultadosNoDisp[$i]); $j++) {
-                        $idReserv=$_resultadosNoDisp[$i][$j];
-                        $sql = "SELECT * FROM reservas WHERE id_Reservas =  $idReserv; " ;
+                    for ($j = 0; $j < count($_resultadosNoDisp[$i]); $j++) {
+                        $idReserv = $_resultadosNoDisp[$i][$j];
+                        $sql = "SELECT * FROM reservas WHERE id_Reservas =  $idReserv; ";
                         $result = $dblink->query($sql);
-                        $infoReserva= $result->fetch();
+                        $infoReserva = $result->fetch();
 
-                        if($j==0){
-                          $sql = "SELECT nombre FROM aulas WHERE id_Aulas =  $infoReserva[1] ; " ;
-                          $result = $dblink->query($sql);
-                          $q= $result->fetch();
+                        if ($j == 0) {
+                            $sql = "SELECT nombre FROM aulas WHERE id_Aulas =  $infoReserva[1] ; ";
+                            $result = $dblink->query($sql);
+                            $q = $result->fetch();
 
                             echo "<td>" . $q[0] . "</td> ";
                         }
 
-                        $sql = "SELECT * FROM usuarios WHERE id_Usuario =  $infoReserva[2] ; " ;
+                        $sql = "SELECT * FROM usuarios WHERE id_Usuario =  $infoReserva[2] ; ";
                         $result = $dblink->query($sql);
-                        $infoUsuario= $result->fetch();
+                        $infoUsuario = $result->fetch();
 
-                        $sql = "SELECT * FROM materias WHERE id_Materias =  $infoReserva[3] ; " ;
+                        $sql = "SELECT * FROM materias WHERE id_Materias =  $infoReserva[3] ; ";
                         $result = $dblink->query($sql);
-                        $infoMaterias= $result->fetch();
-                        if ($j==0) {
-                          echo "<td>";
+                        $infoMaterias = $result->fetch();
+                        if ($j == 0) {
+                            echo "<td>";
                         }
-                        echo "--" . $infoUsuario[1] . " Interno:". $infoUsuario[2] ."<br> Docente: ". $infoReserva[8] ."<br> Materia: " . $infoMaterias[1] . "<br> Fechas: Del " . $infoReserva[4] . " al " . $infoReserva[5]  . "<br>";
-                        if($j==(count($_resultadosNoDisp)-1)){
-                          echo "</td>";
+                        echo "--" . $infoUsuario[1] . " Interno:" . $infoUsuario[2] . "<br> Docente: " . $infoReserva[8] . "<br> Materia: " . $infoMaterias[1] . "<br> Fechas: Del " . $infoReserva[4] . " al " . $infoReserva[5] . "<br>";
+                        if ($j == (count($_resultadosNoDisp) - 1)) {
+                            echo "</td>";
                         }
-                      }
-                      echo "</tr>";
-                                }
-                   ?>
+                    }
+                    echo "</tr>";
+                }
+                ?>
             </table>
-          </div>
-      <br><br><br>
-      </div>
-      <a class="btn btn-primary" href="MotorDeBusqueda.php">Atras</a>
-      <!--boton de informacion-->
-          <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img  src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image" height="42" width="42"  data-target="info"/></button>
-          <div class="modal fade" id="info" name="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
+        </div>
+        <br><br><br>
+    </div>
+    <a class="btn btn-primary" href="MotorDeBusqueda.php">Atras</a>
+    <!--boton de informacion-->
+    <button type="button" class="btn btn-light float-right" data-toggle="modal" data-target="#info"><img
+                src="../Images/iconoInfo.png" onclick="info" class="img-fluid float-right" alt="Responsive image"
+                height="42" width="42" data-target="info"/></button>
+    <div class="modal fade" id="info" name="info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Informacion</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+                    <h5 class="modal-title" id="exampleModalLabel">Informacion</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
-                  En esta pagina se pueden ver las aulas disponibles que cumplen los parametros requeridos
-                  El color verde muestra que se puede realizar la reserva
-                  El color rojo significa que este usuario no tiene el permiso para realizar la reserva
+                    En esta pagina se pueden ver las aulas disponibles que cumplen los parametros requeridos
+                    El color verde muestra que se puede realizar la reserva
+                    El color rojo significa que este usuario no tiene el permiso para realizar la reserva
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
-              </div>
             </div>
-          </div>
+        </div>
     </div>
-  </body>
+</div>
+</body>
 
 </html>
