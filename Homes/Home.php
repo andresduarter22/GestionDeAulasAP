bf85f71304c013fd7d86d8b3d51b6477032de893
Andres <andresduarter13@gmail.com>
2019-01-12 18:35:12 -0400

    ya esta log in con google

--- Homes/Home.php
+++ Homes/Home.php
@@ -15,8 +15,9 @@ $_IDUsuarioTemporal = 3;
 
     <!--Google    -->
     <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id"
-          content="287725469437-1ielsrb28qvp2sv3pa7m1ak8jk9l8816.apps.googleusercontent.com">
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="722692492660-jdifs59bsljp70543hctlv1q0mkjem0u.apps.googleusercontent.com">
+
     <title>Home</title>
 </head>
 <body>
@@ -70,9 +71,24 @@ $_IDUsuarioTemporal = 3;
     <!-- Final boton de Informacion -->
 
     <!-- Log In GOOGLE-->
-    <div class="g-signin2" data-onsuccess="onSignIn">
-        <button type="button" class="btn"> Log in GOOGLE</button>
-    </div>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
+    </script>
+
 
 </body>
 
