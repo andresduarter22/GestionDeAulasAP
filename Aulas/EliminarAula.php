3b251a83b2ef56ddf28ae2a1019bfcbdf3681660
Andres <andresduarter13@gmail.com>
2019-01-20 23:33:04 -0400

    Logs en eliminar aula

--- Aulas/EliminarAula.php
+++ Aulas/EliminarAula.php
@@ -13,7 +13,19 @@
     $sql1= "DELETE FROM Aulas WHERE id_Aulas = ".$_GET['id'].";";
      //echo var_dump($sql1);
     $dblink->query($sql1);
-    $sql_log_ea = "INSERT INTO Logs (id_Log,nombre_usuario,num_interno_usuario,correo_usuario,tipo_usuario,Accion,Fecha_Accion) VALUES (NULL,'Andres','666','ad@gmail.com','m','Se elimino un aula llamada $_nombre',now())";
+    $idDeUsuario = $_SESSION['idUsuario'];
+    $sql_validacion_loggeo = "SELECT * FROM Usuarios where id_Usuario=$idDeUsuario";
+    $info_usuario = $dblink->query($sql_validacion_loggeo);
+    $infoUs = $info_usuario->fetch();
+
+    if ($infoUs['Rol'] == 0) {
+        $rolDeUsuario = "Reservador";
+    } else if ($infoUs['Rol'] == 1) {
+        $rolDeUsuario = "Actualizador";
+    } else {
+        $rolDeUsuario = "Administrador";
+    }
+    $sql_log_ea = "INSERT INTO Logs (id_Log,nombre_usuario,num_interno_usuario,correo_usuario,tipo_usuario,Accion,Fecha_Accion) VALUES (NULL,'" . $infoUs['nombre'] . "','" . $infoUs['num_interno'] . "','" . $infoUs['E_Mail'] . "','" . $rolDeUsuario . "','Se elimino un aula llamada $_nombre',now())";
     $dblink->query($sql_log_ea);
 
    header("Location: GestionDeAulas.php");
